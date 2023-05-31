6d9c0915046b473387c2013dc87778a012fc35f3
wprac <135042772+wprac@users.noreply.github.com>
2023-05-31 09:10:46 +0530

    Add files via upload

---
+++ CC/Assignment1/test.py
@@ -0,0 +1,10 @@
+print ("Content-Type:text/plain")
+print (" ")
+print ("hhhhhhiiii How are you")
+
+#import webapp2
+
+#class MainPage(webapp2, RequestHandler):
+#def get(self):
+#self.response.write("Hello world")
+#app=webapp2.WSGIApplication( ('/',MainPage),debug=True)
